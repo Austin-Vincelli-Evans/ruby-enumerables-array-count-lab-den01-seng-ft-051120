a33f0c34557ccace0242c0854b41ab864079a5c3
Austin Evans <unity7591@gmail.com>
2020-05-01 18:14:10 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,9 +1,9 @@
 def count_strings(array)
   array.count do |element|
-    if element == ""
+    element.string?
 end
 
 def count_empty_strings(array)
-array.count do |element|
+  array.count do |element|
     if element == ""
 end
